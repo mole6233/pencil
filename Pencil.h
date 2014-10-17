af08ce729e450dde199e60e9f62862e4c23b63b8
mole6233 <j.chn@ya.ru>
2014-10-18 00:29:13 +0800

    Update Pencil.h

--- Pencil.h
+++ Pencil.h
@@ -1,16 +1,17 @@
 //
 //  Pencil.h
-//  Created by j.chn@ya.ru on 14-9-30.
-//  Copyright (c) 2014年 j.chn@ya.ru. All rights reserved.
 //
+//  Created by j.chn@ya.ru on 14-9-30.
+//  Copyright (c) 2014年 j.chn. All rights reserved.
+//  将百度坐标转换为GPS or GCJ 坐标
 
 #import <Foundation/Foundation.h>
 #import "BMapKit.h"
 
 @interface Pencil : NSObject
 
-//百度坐标转换成GPS坐标
+-(id)initWithCoordType:(BMK_COORD_TYPE)type;
 
-- (CLLocationCoordinate2D)b2g: (CLLocationCoordinate2D)badCoord;
+- (CLLocationCoordinate2D)b2x: (CLLocationCoordinate2D)bCoord;
 
 @end
